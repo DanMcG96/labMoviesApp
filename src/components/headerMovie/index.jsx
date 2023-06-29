782b30d47faae369ec6be1cea80b32ebca876855
DanMcG96 <20099969@mail.wit.ie>
2023-06-29 21:21:34 +0100

    Show movie review excerpts; Refactor Details page

--- src/components/headerMovie/index.jsx
+++ src/components/headerMovie/index.jsx
@@ -5,6 +5,8 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import Avatar from "@mui/material/Avatar";
+
 
 const styles = {
     root: {  
@@ -19,12 +21,22 @@ const styles = {
 const MovieHeader = (props) => {
   const movie = props.movie;
 
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
-      </IconButton>
-
+      </IconButton> 
+      <Typography variant="h4" component="h3">
+        
+      </Typography>
+      
       <Typography variant="h4" component="h3">
         {movie.title}{"   "}
         <a href={movie.homepage}>
