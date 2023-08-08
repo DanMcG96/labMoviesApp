2993606304e4279f832453b20e679acfaa5511f7
DanMcG96 <20099969@mail.wit.ie>
2023-08-08 11:11:33 +0100

    Added Upcoming Movies Page.

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import MovieReviewPage from "./pages/movieReviewsPage";
 import SiteHeader from "./components/siteHeader";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
+
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,6 +28,7 @@ const App = () => {
       <SiteHeader /> {/* New Header */}
       <Routes>
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
---
+++ src/pages/upcomingMoviesPage.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from "../components/spinner";
+
+const UpcomingMoviesPage = (props) => {
+  const { data, error, isLoading, isError } = useQuery("movies", getUpcomingMovies);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const movies = data ? data.results : [];
+
+  // Redundant, but necessary to avoid app crashing.
+  const favourites = movies.filter((m) => m.favorite);
+  localStorage.setItem("favourites", JSON.stringify(favourites));
+  const addToFavourites = (movieId) => true;
+
+  return (
+    <PageTemplate
+      title="Coming Soon"
+      movies={movies}
+      selectFavourite={addToFavourites}
+    />
+  );
+};
+export default UpcomingMoviesPage;
